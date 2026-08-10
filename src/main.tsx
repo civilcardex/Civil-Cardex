762ec54f8262a8dfb5a172f6f1c73079d199f6ef
civilcardex <civilcardex@gmail.com>
2026-08-10 17:18:50 -0500

    feat(analytics): add Vercel Speed Insights
    Install @vercel/speed-insights and mount SpeedInsights in the app root.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>,
-)
+);
